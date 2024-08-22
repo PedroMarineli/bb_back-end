f86a46eab00494d3adf26312034a3bafaf09acb4
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2024-08-22 15:30:36 -0300

    implementar o menu de terminal.

--- src/main/java/br/com/fatec/burguerboss/principal/Principal.java
+++ src/main/java/br/com/fatec/burguerboss/principal/Principal.java
@@ -1,6 +1,9 @@
 package br.com.fatec.burguerboss.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+    Scanner read = new Scanner(System.in);
     public void showMenu() {
         var option = -1;
 
@@ -9,11 +12,16 @@ public class Principal {
                         *|Burguer Boss|*
                         
                         1- Manter mesas
+                                       
+                        
+                        99- Sair
                     """;
 
             System.out.println(menu);
+
+            System.out.print("Opcao escolhida: ");
+            option = read.nextInt();
+
         }
-
-
     }
 }
