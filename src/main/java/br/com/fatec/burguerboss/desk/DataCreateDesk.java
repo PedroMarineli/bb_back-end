78a2472872b9f88282276f04f009c4b422e40103
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2024-10-03 15:56:39 -0300

    BUR-4 validação dos endpoints feitos

--- src/main/java/br/com/fatec/burguerboss/desk/DataCreateDesk.java
+++ src/main/java/br/com/fatec/burguerboss/desk/DataCreateDesk.java
@@ -1,6 +1,9 @@
 package br.com.fatec.burguerboss.desk;
 
+import jakarta.validation.constraints.NotNull;
+
 public record DataCreateDesk(
+        @NotNull
         int deskNumber
 ) {
 }
--- src/main/java/br/com/fatec/burguerboss/desk/DataUpdateDesk.java
+++ src/main/java/br/com/fatec/burguerboss/desk/DataUpdateDesk.java
@@ -1,6 +1,9 @@
 package br.com.fatec.burguerboss.desk;
 
+import jakarta.validation.constraints.NotNull;
+
 public record DataUpdateDesk(
+        @NotNull
         int id
 ) {
 
