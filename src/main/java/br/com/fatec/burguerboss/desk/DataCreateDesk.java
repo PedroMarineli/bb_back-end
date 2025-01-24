8832c517c580d1a1371b2b6f971d7343ada5952e
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-01-24 08:28:34 -0300

    desk validation completed

--- src/main/java/br/com/fatec/burguerboss/desk/DataCreateDesk.java
+++ src/main/java/br/com/fatec/burguerboss/desk/DataCreateDesk.java
@@ -1,9 +1,13 @@
 package br.com.fatec.burguerboss.desk;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record DataCreateDesk(
-        @NotNull(message = "this field cannot be null")
+        @NotNull
+        @Min(0)
+        @Max(99)
         Integer deskNumber
 ) {
 }
--- src/main/java/br/com/fatec/burguerboss/desk/DataUpdateDesk.java
+++ src/main/java/br/com/fatec/burguerboss/desk/DataUpdateDesk.java
@@ -1,9 +1,12 @@
 package br.com.fatec.burguerboss.desk;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record DataUpdateDesk(
         @NotNull
+        @Min(0)
         Integer id
 ) {
 
