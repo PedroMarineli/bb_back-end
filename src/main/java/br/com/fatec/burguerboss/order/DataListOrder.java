38ae0a248bc72673eee240a196d4de32c1fd760a
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-01-27 16:08:27 -0300

    BUR-14 list order completed

---
+++ src/main/java/br/com/fatec/burguerboss/order/DataListOrder.java
@@ -0,0 +1,27 @@
+package br.com.fatec.burguerboss.order;
+
+import br.com.fatec.burguerboss.desk.Desk;
+import br.com.fatec.burguerboss.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record DataListOrder(
+        Integer id,
+        BigDecimal totalValue,
+        PaymentMethod paymentMethod,
+        List<OrderItem> orderItems,
+        OrderStatus orderStatus,
+        Desk desk
+) {
+    public DataListOrder(Order order){
+        this(
+                order.getId(),
+                order.getTotalValue(),
+                order.getPaymentMethod(),
+                order.getOrderItems(),
+                order.getOrderStatus(),
+                order.getDesk()
+        );
+    }
+}
---
+++ src/main/java/br/com/fatec/burguerboss/order/OrderService.java
@@ -0,0 +1,20 @@
+package br.com.fatec.burguerboss.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    public List<DataListOrder> listOrders() {
+        return orderRepository.findAll().stream().map(DataListOrder::new).collect(Collectors.toList());
+    }
+}
