7df4d43aa4c55280ff602b566fc9e10206e26e27
Pedro <pedrohenrique.marineli@gmail.com>
2025-03-27 19:54:01 -0300

    BUR-10 começando a implementar o spring security

---
+++ src/main/java/br/com/fatec/burguerboss/infra/security/configuration/JwtTokenProvider.java
@@ -0,0 +1,50 @@
+package br.com.fatec.burguerboss.infra.security.configuration;
+
+import com.sun.security.auth.UserPrincipal;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private String jwtSecret = "seuSegredoSuperSecreto";
+    private int jwtExpirationInMs = 3600000; // 1 hora
+
+    public String generateToken(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userPrincipal.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception ex) {
+            // Log error
+        }
+        return false;
+    }
+}
