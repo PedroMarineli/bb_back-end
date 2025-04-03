ab85e473858d20c5acbf680f7ebc1067529cb26e
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-04-03 12:15:46 -0300

    BUR-10 modificando a filterChain

--- src/main/java/br/com/fatec/burguerboss/domain/user/UserRepository.java
+++ src/main/java/br/com/fatec/burguerboss/domain/user/UserRepository.java
@@ -2,8 +2,10 @@ package br.com.fatec.burguerboss.domain.user;
 
 import br.com.fatec.burguerboss.domain.menu.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<MenuItem, Integer> {
+    UserDetails findByUsername(String login);
 }
--- src/main/java/br/com/fatec/burguerboss/infra/security/configuration/SecurityConfig.java
+++ src/main/java/br/com/fatec/burguerboss/infra/security/configuration/SecurityConfig.java
@@ -1,28 +1,21 @@
+package br.com.fatec.burguerboss.infra.security.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+public class SecurityConfig{
 
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
+
 }
