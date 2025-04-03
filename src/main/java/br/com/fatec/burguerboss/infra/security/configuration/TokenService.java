0561c622cc3f79a7ecfe324a5cc5809f056f4f82
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-04-03 16:50:34 -0300

    BUR-10 adicionar secret no app properties

--- src/main/java/br/com/fatec/burguerboss/infra/security/configuration/TokenService.java
+++ src/main/java/br/com/fatec/burguerboss/infra/security/configuration/TokenService.java
@@ -4,6 +4,7 @@ import br.com.fatec.burguerboss.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,9 +14,12 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(User user) {
         try {
-            var algoritmo = Algorithm.HMAC256("12345678");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API burguerboss")
                     .withSubject(user.getUsername())
